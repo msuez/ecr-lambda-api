9113b878162bef2959b3fa108d848d715fafc7ff
msuez <matisuez@gmail.com>
2024-11-26 15:46:44 -0300

    Update without swagger-jsdoc

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
+import path from 'path';
+import YAML from 'yamljs';
 import express from 'express';
 import serverless from 'serverless-http';
 import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './config/swagger';
+
 import routes from './routes/';
 
 const app = express();
+const swaggerDocument = YAML.load(path.resolve(__dirname,'./config/swagger.yaml'));
 
 app.use(express.json());
 
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', routes);
 
 export const handler = serverless(app);
+export default app;
