e56b7fe438454114b120be8f852985647127df58
msuez <matisuez@gmail.com>
2024-11-26 16:36:13 -0300

    Add public folder with static files

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
+import path from 'path';
+import cors from 'cors';
 import express from 'express';
+import favicon from 'serve-favicon';
 import serverless from 'serverless-http';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './config/swagger';
@@ -6,12 +9,25 @@ import routes from './routes/';
 
 const app = express();
 
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+);
+app.use(favicon(path.join(__dirname, '../public', 'favicon-32x32.png')));
+
 app.use(express.json());
 
-// Ruta para la documentación de Swagger
+app.use('/static', express.static(path.join(__dirname, '../public'), {
+    setHeaders: (res) => {
+        res.setHeader('Cache-Control', 'public, max-age=31536000');
+    },
+}));
+
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Rutas de la API
 app.use('/api', routes);
 
 export const handler = serverless(app);
