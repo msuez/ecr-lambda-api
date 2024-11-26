8ebd08b804317c6d69bdc95abc89a10bc17d0ae8
msuez <matisuez@gmail.com>
2024-11-26 14:57:20 -0300

    Update api route

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import serverless from 'serverless-http';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './config/swagger';
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api', routes);
 
 export const handler = serverless(app);
